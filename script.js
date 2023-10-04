a6934fa5b6f80c6adfd9eaf86a6b1cb413186060
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2023-10-04 20:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+ 
+// js code
+ 
+ 
 function getFormvalue() {
     //Write your code here
-
+let first =document.querySelector('#form1 input[name="fname"]');
+let last =document.querySelector('#form1 input[name="lname"]');
+	let fname=first.value;
+	let lname=last.value;
+	alert(fname+" "+lname);
 }
